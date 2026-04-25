5240f039e2194a3ebf0bb2d4e548cc4f0783cc27
gnosticDeveloper <gnosticdev.studio@proton.me>
2026-04-25 18:48:30 -0300

    initial commit

---
+++ services/raum/src/main/java/raum/RaumApplication.java
@@ -0,0 +1,13 @@
+package raum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaumApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RaumApplication.class, args);
+	}
+
+}
---
+++ services/raum/src/test/java/raum/RaumApplicationTests.java
@@ -0,0 +1,13 @@
+package raum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
