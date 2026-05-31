8eb7817cf331f6cbef2067d5e9f6e69444c94509
gnosticDeveloper <gnosticdev.studio@proton.me>
2026-05-31 19:58:01 -0300

    Added sql file for user entity. To be executed manually inside of configured connection for vassago service (#18)

---
+++ services/vassago/src/main/resources/users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+                                             id                 uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+                                             name               varchar(255) NOT NULL,
+                                             last_name          varchar(255) NOT NULL,
+                                             email              varchar(255) NOT NULL UNIQUE,
+                                             username           varchar(255) NOT NULL UNIQUE,
+                                             password           varchar(255) NOT NULL,
+                                             roles              text NOT NULL,
+                                             modification_lock  bool DEFAULT false,
+                                             locked_at          timestamp,
+                                             created_at         timestamp DEFAULT current_timestamp,
+                                             modified_at        timestamp DEFAULT current_timestamp,
+                                             stopped_at         timestamp default null
+);
