e7a6dac49ef383c36872a21bc71885023f6857ef
gnosticDeveloper <gnosticdev.studio@proton.me>
2026-06-07 19:43:46 -0300

    attempted fix by way of controlling when tests execute

--- services/vassago/src/test/java/vassago/CreateUserIT.java
+++ services/vassago/src/test/java/vassago/CreateUserIT.java
@@ -52,7 +52,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-class CreateUserIntegrationTest {
+class CreateUserIT {
 
     static final Network network = Network.newNetwork();
 
